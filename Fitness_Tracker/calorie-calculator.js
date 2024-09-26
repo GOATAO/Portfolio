22f65b7a7e682c2061dc40daa1b7dc9427d6b04e
Talal Abuziandah <primezinadah@gmail.com>
2024-09-26 10:20:13 +0100

    Refrence needed elements

---
+++ Fitness_Tracker/calorie-calculator.js
@@ -0,0 +1,13 @@
+//getting all needed elements//
+const maleInput = document.getElementById("male-input");
+const femaleInput = document.getElementById("female-input");
+const otherInput = document.getElementById("other-input");
+
+const ageInput = document.getElementById("age-input");
+const heightInput = document.getElementById("height-input");
+const weightInput = document.getElementById("weight-input");
+
+const freqInput = document.getElementById("training-frequency");
+
+const calcBtn = document.getElementsByName("calculate-btn");
+const clearBtn = document.getElementById("clear-btn");
