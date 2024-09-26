9b3e28104e5e4354d390c636c06954932817131c
Talal Abuziandah <primezinadah@gmail.com>
2024-09-26 10:47:07 +0100

    Implement basic button functionality

--- Fitness_Tracker/calorie-calculator.js
+++ Fitness_Tracker/calorie-calculator.js
@@ -1,4 +1,5 @@
 //getting all needed elements//
+const caloriesForm = document.getElementById("calories-form")
 const maleInput = document.getElementById("male-input");
 const femaleInput = document.getElementById("female-input");
 const otherInput = document.getElementById("other-input");
@@ -9,7 +10,7 @@ const weightInput = document.getElementById("weight-input");
 
 const freqInput = document.getElementById("training-frequency");
 
-const calcBtn = document.getElementsByName("calculate-btn");
+const calcBtn = document.getElementById("calculate-btn");
 const clearBtn = document.getElementById("clear-btn");
 
 const bmrOutput = document.getElementById("bmrSpan");
@@ -18,3 +19,25 @@ const fatOutput = document.getElementById("fatSpan");
 
 //getting the calories elements//
 const caloriesOutput = document.querySelectorAll(".calories-span");
+
+
+// function to handle when calculate is pressed
+function calculate(e){
+    e.preventDefault();
+    if(caloriesForm.checkValidity()){
+        console.log("calculating");
+    } else{
+        caloriesForm.reportValidity();
+    }
+}
+
+// function to handle when clear is pressed
+function clear(e){
+    e.preventDefault();
+    console.log("clearing inputs");
+}
+
+//adding button functions
+calcBtn.addEventListener("click", calculate);
+clearBtn.addEventListener("click", clear);
+
